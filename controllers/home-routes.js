651f6cabfef0d09bdecb7fd631fa9101f1e8bdc0
Susanne85 <xkqk2010@gmail.com>
2021-06-13 00:41:29 +0800

    Added views and controllers for login and signup

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,58 +1,28 @@
 const router = require('express').Router();
-const { Blog, Comment } = require('../models');
-const withAuth = require('../utils/auth');
-// GET all galleries for homepage
-router.get('/', withAuth, async (req, res) => {
-  try {
-    // attributes: ['blog_id', 'post_content'],
-    const dbBlogData = await Blog.findAll({
-      //   include: [
-      //     Comment,
-      //  ],
-    });
 
-    const blogs = dbBlogData.map((blogData) =>
-      blogData.get({ plain: true })
-    );
-    console.log('here', blogs);
-    console.log('comment', blogs[0].comments);
-    res.render('homepage', {
-      blogs,
-      loggedIn: req.session.loggedIn,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+router.get('/', async (request, response) => {
+  if (request.session.loggedIn) {
+    response.redirect('/dashboard');
+    return;
   }
+  response.render('homepage');
 });
 
-// GET one gallery
-// TODO: Replace the logic below with the custom middleware
-router.get('/comment/:id', withAuth, async (req, res) => {
-  // Checking whether the user is logged in and resulting navigation is done within withAuth
-  try {
-    const dbBlogData = await Blog.findByPk(req.params.id, {
-      include: [
-        Comment,
-      ],
-    });
-    const blog = dbBlogData.get({ plain: true });
-    console.log ('in get for a blog', blog);
-    res.render('comment', { blog,  loggedIn: req.session.loggedIn });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+router.get('/login', async (request, response) => {
+  if (request.session.loggedIn) {
+    response.redirect('/dashboard');
+    return;
   }
+  response.render('login');
 });
 
-
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
+router.get('/signup', async (request, response) => {
+  if (request.session.loggedIn) {
+    response.redirect('/dashboard');
     return;
   }
 
-  res.render('login');
+  response.render('signup');
 });
 
 module.exports = router;
---
+++ public/js/signup.js
@@ -0,0 +1,20 @@
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ username, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    document.location.replace('/dashboard');
+  } else {
+    alert('User already exists, please login');
+  }
+};
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
