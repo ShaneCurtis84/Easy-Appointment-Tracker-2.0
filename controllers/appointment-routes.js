05f9e3f20b8a7fac07bf09228fcc1a1cda76c3dc
Susanne85 <xkqk2010@gmail.com>
2021-06-17 11:23:40 +0800

    M30.10 - Added functionality to search appointments by date

--- controllers/appointment-routes.js
+++ controllers/appointment-routes.js
@@ -5,10 +5,10 @@ const withAuth = require('../utils/auth');
 // Get the add new appointment page
 router.get('/new', withAuth, async (request, response) => {
   try {
-      response.render('add-new-appointment', { loggedIn: request.session.loggedIn });
+    response.render('add-new-appointment', { loggedIn: request.session.loggedIn });
   } catch (err) {
-      console.log(err);
-      response.status(500).json(err);
+    console.log(err);
+    response.status(500).json(err);
   }
 });
 
@@ -28,18 +28,32 @@ router.get('/daily-itinerary', withAuth, async (request, response) => {
     response.status(500).json(error);
   }
 });
+router.get('/search-appointments', withAuth, async (request, response) => {
+  console.log('Appointment Routes -search appointment');
+  try {
+
+    response.render('search-appointments', {
+      loggedIn: request.session.loggedIn,
+    });
+
+  } catch (error) {
+    response.status(500).json(error);
+  }
+});
 router.get('/view-appointments', withAuth, async (request, response) => {
   try {
     const dbAppointmentData = await Appointment.findAll({
     });
+    const searchDateFrom = request.query.startDate;
+    const searchDateTo = request.query.endDate;
 
-    const appointments = dbAppointmentData.map((appointmentData) =>
-      appointmentData.get({ plain: true })
-    );
-    response.render('appointment', {
-      appointments,
-      loggedIn: request.session.loggedIn,
-    });
+    const appointmentsFiltered = dbAppointmentData.filter(appntDate => appntDate.appnt_date >= searchDateFrom && appntDate.appnt_date <= searchDateTo);
+
+    const appointments = appointmentsFiltered.sort((a,b) => new Date(a.appnt_date) - new Date(b.appnt_date)).map((appointmentData) =>
+    appointmentData.get({ plain: true }));
+
+    response.render('appointment', {appointments, loggedIn: request.session.loggedIn, });
+    
   } catch (error) {
     response.status(500).json(error);
   }
